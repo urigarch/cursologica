0987f4eca12fa8dd3597bcb9949c6290d4c6b166
urigarch <104804322+urigarch@users.noreply.github.com>
2022-05-02 20:01:43 -0500

    Primera actualización

---
+++ Python curso/HolaMundo.py
@@ -0,0 +1,10 @@
+#COMENTARIO: Ejercicio de un saludo
+#Los comentarios sirven para documentar un codigo o programa
+# y los omite o no los toma en cuenta python
+#IMPRIMIR EN PANTALLA O MOSTRAR LA SALIDA EN PANTALLA
+
+print("HOLA MUNDO")
+print('Uriel Garcia Chavarria');
+print(5+5)
+print(10-2)
+print("CURSO DE LOGICA")

2efa77a9681e50513f3875b7b5d8a477c5173b34
urigarch <104804322+urigarch@users.noreply.github.com>
2022-05-04 19:25:59 -0500

    nuevos ejercicios

---
+++ Area_Peri_circulo.py
@@ -0,0 +1,16 @@
+# CALCULAR EL AREA Y PERIMETRO DE UN CIRCULO
+# ENTRADAS DE DATOS
+# DEFINIR LAS VARIABLES DE ENTRADA
+
+pi = 3.1416
+radio = float (input("introduce el radio del circulo"))
+
+# AREA DEL CIRCULO 
+# PERIMETRO DEL CIRCULO
+
+area = pi*(radio**2)
+peri=2*radio*pi
+
+# SALIDA DE DATOS
+print("area=",area)
+print("perimetro=",peri)
---
+++ Edad.py
@@ -0,0 +1,11 @@
+# DETERMINA LA EDAD DE UNA PERSONA
+# CONOCIENDO SU AÑO ACTUAL Y EL AÑO DE NACIMIENTO
+
+#DATOS
+año_actual = int(input("ingresar el año actual"))
+año_nacimiento = int(input("ingresar su año de nacimiento"))
+# PROCESOS 
+edad = año_actual - año_nacimiento
+#  SALIDA DE DATOS
+
+print("su edad es de", edad, "años")

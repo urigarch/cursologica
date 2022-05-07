8ada60fc1c8ebd0217fab9fb2afdb35c27e7d3c0
urigarch <104804322+urigarch@users.noreply.github.com>
2022-05-06 21:13:25 -0500

    Add files via upload

---
+++ Numero_par_impar.py
@@ -0,0 +1,14 @@
+# PEDIR SI UN NUMERO ES PAR O IMPAR
+from __future__ import division
+
+
+
+numero = int(input("ingresa un numero: " ))
+
+
+
+if numero%2==0:
+    print(" valor es par")
+
+else:
+    print("valor es impar")

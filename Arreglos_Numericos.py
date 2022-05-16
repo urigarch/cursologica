1ad9837d81bedb50cb488878157bcd05c808befc
urigarch <104804322+urigarch@users.noreply.github.com>
2022-05-16 17:38:10 -0500

    Add files via upload

---
+++ Arreglos_Numericos.py
@@ -0,0 +1,15 @@
+# Hacer una función para que imprima un arreglo o lista de 10 elementos numéricos
+
+
+# ENTRADA DE DATOS: Declarar las listas o arreglos
+
+Numeros =  [100, 80, 30, 20, 150, 60, 40, 70, 5, 5]
+
+# Arreglar un elemento
+Numeros.append(10)
+
+''' Ordenar los elementos de un arreglo '''
+Numeros.sort() #ordena numeros ascendente
+
+# SALIDA DE DATOS"
+print(Numeros) 
